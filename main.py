ad7f980267ee462b965e7a5c5830917899f4eb85
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2022-06-28 00:50:37 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import requests
 from pytgcalls import idle
 from callsmusic import run
 from handlers import __version__
-from handler.nopm.import User
+from pyrogram import Client as Bot
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from pyrogram import Client
+
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
     file.write(response.content)
 
 
-bot = Client(
+bot = Bot(
     ":memory:",
     API_ID,
     API_HASH,
@@ -19,11 +19,8 @@ bot = Client(
     plugins=dict(root="handlers"),
 )
 
-Bot.start()
-User.start()
-print("\n[INFO] - STARTED VIDEO PLAYER BOT, JOIN @BOT_X_SUPPORT !")
+print(f"[INFO]: VEEZ MUSIC v{__version__} STARTED !")
 
+bot.start()
+run()
 idle()
-Bot.stop()
-User.stop()
-print("\n[INFO] - STOPPED VIDEO PLAYER BOT, JOIN @BOT_X_CHANNEL")
